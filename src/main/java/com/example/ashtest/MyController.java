6c3dce65ec996bc2aad02532dbcc59693e915547
chickenchickenlove <ojt90902@naver.com>
2023-06-19 22:52:19 +0900

    k8s in action chapter 14 study

--- src/main/java/com/example/ashtest/MyController.java
+++ src/main/java/com/example/ashtest/MyController.java
@@ -8,34 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class MyController {
-
     @GetMapping("/")
     public String hello() {
-
+        System.out.println("hello ash");
         log.trace("hello info");
         log.debug("hello info");
         log.info("hello info");
         log.warn("hello info");
         log.error("hello info");
-
         return "ok";
     }
-
-
-//    private static int errorCount = 0;
-
-//    @GetMapping("/health-check")
-//    public String hello(HttpServletResponse response) {
-//        log.info("errorCount = {}", errorCount);
-//        errorCount++;
-//        if (errorCount % 5 == 0) {
-//            response.setStatus(500);
-//        } else {
-//            response.setStatus(200);
-//        }
-//        return "error";
-//    }
-
-
-
 }
